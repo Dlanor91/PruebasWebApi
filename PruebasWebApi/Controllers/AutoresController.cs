04ef5e6e628e7b10b498db721a3139e6aa97ff0b
Dlanor91 <limaronald926@gmail.com>
2022-06-09 22:26:56 -0300

    Controller +Api

--- PruebasWebApi/Controllers/AutoresController.cs
+++ PruebasWebApi/Controllers/AutoresController.cs
@@ -8,40 +8,59 @@ using System.Threading.Tasks;
 
 namespace PruebasWebApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("Customers")]//cambio la ruta ("api/[controller]") 
     [ApiController]
     public class AutoresController : ControllerBase
     {
-        // GET: api/<AutoresController>
+        // GET: api/<AutoresController>/FindAll
+        [Route ("FindAll")]//ruteo
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            return Ok(new List<Autor>()
+            {
+                new Autor() { Id =1, Nombre = "Juan" , Apellido = "Perez"},
+                new Autor() { Id =2, Nombre = "Ana" , Apellido = "Gomez"}
+            });
         }
 
-        // GET api/<AutoresController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        // GET api/<AutoresController>/FindById/5
+        [Route("FindById/{id}/Nombre/{nombre}")]//ruteo y agrego el id
+        [HttpGet] //se saca el id y se ponen encima los dos parametros
+        public IActionResult Get(int id, string nombre)
         {
-            return "value";
+            return Ok(new Autor() { Id =id, Nombre = nombre, Apellido = "Perez" });
         }
 
         // POST api/<AutoresController>
         [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        public IActionResult Post([FromBody] Autor a)//para que devuelva 201 
+        {       
+            //si no es valido
+           // return BadRequest(); //si es valido
+
+            //si es valido
+            return Created("api/autores/"+a.Id,a);//Recibe URI que es ruta pra el find by id, objeto
+
+            //Si ocurre un error
+            //return StatusCode(500);
+
+            //si no se pudo hacer el alta
+            //return Conflict();
         }
 
         // PUT api/<AutoresController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(int id, [FromBody] Autor a)
         {
+            return NoContent();
         }
 
         // DELETE api/<AutoresController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            return NoContent();
         }
     }
 }
